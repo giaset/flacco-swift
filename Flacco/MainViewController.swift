7e33dc197112a7f67efcd3d71ae58963612cebd5
Gianni Settino <g@miltonandparc.com>
2014-11-14 17:32:04 -0500

    Yellow

--- Flacco/MainViewController.swift
+++ Flacco/MainViewController.swift
@@ -11,23 +11,23 @@ import UIKit
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.lightGrayColor()
+        super.viewDidLoad()        
+        view.backgroundColor = UIColor.whiteColor()
         
         var statusBarView = UIView(frame: CGRect(x: 0, y: 0, width: CGRectGetWidth(view.frame), height: 40))
-        statusBarView.backgroundColor = UIColor(red: 0.6, green: 0.2, blue: 0.6, alpha: 1)
+        statusBarView.backgroundColor = UIColor.blackColor()
         view.addSubview(statusBarView)
         
         var titleLabel = UILabel()
         titleLabel.text = "FLACCO"
-        titleLabel.font = UIFont.boldSystemFontOfSize(13)
-        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = UIFont(name: "Verdana-Bold", size: 13)
+        titleLabel.textColor = UIColor(red: 1, green: 0.8, blue: 0, alpha: 1)
         titleLabel.sizeToFit()
         titleLabel.center = CGPoint(x: view.center.x, y: 30)
         view.addSubview(titleLabel)
         
         var previewView = UIView(frame: CGRect(x: 0, y: 40, width: CGRectGetWidth(view.frame), height: CGRectGetWidth(view.frame)))
-        previewView.backgroundColor = UIColor.blackColor()
+        previewView.backgroundColor = UIColor.lightGrayColor()
         view.addSubview(previewView)
     }
     
