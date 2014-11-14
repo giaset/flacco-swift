6eec138c218c63919856b6dc44ac94fa657d3903
Gianni Settino <g@miltonandparc.com>
2014-11-14 11:52:09 -0500

    Basic project setup

---
+++ Flacco/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Flacco
+//
+//  Created by Gianni Settino on 2014-11-14.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.whiteColor()
+    }
+    
+}
+
