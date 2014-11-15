f0f7029117e38766a6a7ba5242f464fcd5aa91fd
Gianni Settino <g@miltonandparc.com>
2014-11-14 23:04:04 -0500

    Sexy record button

--- Flacco/MainViewController.swift
+++ Flacco/MainViewController.swift
@@ -28,6 +28,25 @@ class MainViewController: UIViewController {
         var bottomBlurView = UIVisualEffectView(effect: blurEffect)
         bottomBlurView.frame = CGRect(x: 0, y: view.bounds.size.height-topAndBottomViewHeight, width: previewSquareSize, height: topAndBottomViewHeight)
         view.addSubview(bottomBlurView)
+        
+        var recordButtonRadius: CGFloat = 45
+        var recordButtonBorderOffset: CGFloat = 10
+        
+        var recordButtonBorder = UIView(frame: CGRectMake(0, 0, 2*(recordButtonRadius+recordButtonBorderOffset), 2*(recordButtonRadius+recordButtonBorderOffset)))
+        recordButtonBorder.backgroundColor = UIColor.clearColor()
+        recordButtonBorder.layer.borderColor = UIColor.whiteColor().CGColor
+        recordButtonBorder.layer.borderWidth = recordButtonBorderOffset/2
+        recordButtonBorder.layer.masksToBounds = true
+        recordButtonBorder.layer.cornerRadius = recordButtonRadius+recordButtonBorderOffset
+        recordButtonBorder.center = CGPoint(x: view.center.x, y: bottomBlurView.center.y)
+        view.addSubview(recordButtonBorder)
+        
+        var recordButton = UIButton(frame: CGRectMake(0, 0, 2*recordButtonRadius, 2*recordButtonRadius))
+        recordButton.layer.masksToBounds = true
+        recordButton.layer.cornerRadius = recordButtonRadius
+        recordButton.backgroundColor = UIColor.whiteColor()
+        recordButton.center = CGPoint(x: view.center.x, y: bottomBlurView.center.y)
+        view.addSubview(recordButton)
     }
     
     override func viewWillAppear(animated: Bool) {
