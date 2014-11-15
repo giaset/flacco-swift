fc42ff3fb3ede69815432435be816e06e0f24c25
Gianni Settino <g@miltonandparc.com>
2014-11-14 22:32:12 -0500

    Blur views!

--- Flacco/MainViewController.swift
+++ Flacco/MainViewController.swift
@@ -14,27 +14,25 @@ class MainViewController: UIViewController {
         super.viewDidLoad()        
         view.backgroundColor = UIColor.whiteColor()
         
-        var statusBarView = UIView(frame: CGRect(x: 0, y: 0, width: CGRectGetWidth(view.frame), height: 40))
-        statusBarView.backgroundColor = UIColor.blackColor()
-        view.addSubview(statusBarView)
-        
-        var titleLabel = UILabel()
-        titleLabel.text = "FLACCO"
-        titleLabel.font = UIFont(name: "Helvetica-Bold", size: 13)
-        titleLabel.textColor = UIColor(red: 1, green: 0.8, blue: 0, alpha: 1)
-        titleLabel.sizeToFit()
-        titleLabel.center = CGPoint(x: view.center.x, y: 30)
-        view.addSubview(titleLabel)
-        
-        var previewView = CaptureView(frame: CGRect(x: 0, y: 40, width: CGRectGetWidth(view.frame), height: CGRectGetWidth(view.frame)))
-        previewView.backgroundColor = UIColor.lightGrayColor()
+        var previewView = CaptureView(frame: view.bounds)
         view.addSubview(previewView)
+        
+        var previewSquareSize = view.bounds.size.width
+        var topAndBottomViewHeight = (view.bounds.size.height-previewSquareSize)/2
+        
+        var topBlurView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+        topBlurView.frame = CGRect(x: 0, y: 0, width: previewSquareSize, height: topAndBottomViewHeight)
+        view.addSubview(topBlurView)
+        
+        var bottomBlurView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+        bottomBlurView.frame = CGRect(x: 0, y: view.bounds.size.height-topAndBottomViewHeight, width: previewSquareSize, height: topAndBottomViewHeight)
+        view.addSubview(bottomBlurView)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
-        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        UIApplication.sharedApplication().statusBarHidden = true
     }
     
 }
